75eecc23b334469bd0922d961eb5f9286d1a9963
Janviermbilizi <janviermbilizi@gmail.com>
2021-07-07 21:10:55 -0400

    v1.0.0 with Modal, ModalLink, ModalRoute, ModalRouteContent, ModalSwitch and useModalGallery.

---
+++ sutwa-react-modal/ModalLink.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export function splitPathnameAndQueryString(path) {
+  const [pathname, search] = path.split("?");
+
+  return {
+    pathname,
+    search: search ? `?${search}` : "",
+  };
+}
+
+function addModalStateToLink(to) {
+  if (typeof to === "string") {
+    const { pathname, search } = splitPathnameAndQueryString(to);
+    return {
+      pathname,
+      search: search,
+      state: {
+        modal: true,
+      },
+    };
+  } else {
+    return {
+      ...to,
+      state: {
+        ...to.state,
+        modal: true,
+      },
+    };
+  }
+}
+
+function ModalLink({ to, ...rest }) {
+  return <Link {...rest} to={addModalStateToLink(to)} />;
+}
+
+ModalLink.propTypes = Link.propTypes;
+
+export default ModalLink;
---
+++ sutwa-react-modal/index.js
@@ -0,0 +1,6 @@
+export { default as Modal } from "./Modal";
+export { default as ModalRoute } from "./ModalRoute";
+export { default as ModalSwitch } from "./ModalSwitch";
+export { default as ModalLink } from "./ModalLink";
+export { default as ModalRouteContent } from "./ModalRouteContent";
+export { default as useModalGallery } from "./useModalGallery";
